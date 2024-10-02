5724987b7924b72469886bc4bd9c1f9fb99d49c5
Chidam Gopal <cgopal@mozilla.com>
2024-10-02 16:08:39 -0400

    indentation

--- src/js/intent.js
+++ src/js/intent.js
@@ -106,7 +106,7 @@ function sumObjectsByKey(...objs) {
             if (b.hasOwnProperty(k))
                 a[k] = (a[k] || 0) + b[k];
         }
-      return a;
+        return a;
     }, {});
 }
 
