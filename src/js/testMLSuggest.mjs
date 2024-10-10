8214989a25deca2165b11e1f19fd74a4424d648d
Chidam Gopal <cgopal@mozilla.com>
2024-10-10 14:53:37 -0400

    updates to MLSuggest example

--- src/js/testMLSuggest.mjs
+++ src/js/testMLSuggest.mjs
@@ -1,15 +1,16 @@
 import MLSuggest from './MLSuggest.sys.mjs';
 
-async function testMLSuggest() {
+async function testMLSuggest(query) {
     // Initialize the ML models (optional if models need to be loaded in advance)
     await MLSuggest.initialize();
 
     // Make ML suggestions for a query
-    const result = await MLSuggest.makeMLSuggestions("restaurants in seattle");
+    const result = await MLSuggest.makeMLSuggestions(query);
     console.log(result);
 
     // // Shutdown the engines after use
     // await MLSuggest.shutdown();
 }
 
-testMLSuggest();
+query = "restaurants in seattle"
+testMLSuggest(query);
